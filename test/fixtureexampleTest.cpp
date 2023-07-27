192eec920e3e44dde7d380eb943ba5b592718fb8
srt3cob <s.sarathy@in.bosch.com>
2023-07-27 11:14:23 +0530

    test fixture

---
+++ test/fixtureexampleTest.cpp
@@ -0,0 +1,31 @@
+
+#include "gtest"
+class a(){
+    b*ptr;
+    public:
+    a(b*adddressobject): ptr(addressofobject){
+
+    }
+Bool operation(){
+    return true;
+}
+};
+class b();
+c*ptr;
+    public:
+    c(b*adddressobject): ptr(addressofobject){
+
+    }
+
+class c();
+a*ptr;
+    public:
+    a(b*adddressobject): ptr(addressofobject){
+
+    }
+TEST("testfixturetestsuit", "operationtest"){
+    c cobj;
+    b bobj(&cobj);
+    a obj(&bobj);
+    ASSERT_FALSE(obj.operetion());
+}
